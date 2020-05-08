e3ffe5055972cc20be16e9b49a2227ecba33f1dd
Ruslan101 <ttracer64@gmail.com>
2020-05-08 12:27:39 +0300

    Include Webpack-merge for separation of configuration files:     ./config/webpack.base.conf.js - main configuration file     ./config/webpack.build.conf.js - project build file (use npm run build for this)     ./config/webpack.dev.conf.js - development file. setting for webpack server (use npm run dev for this)
    add map site
    add live-reload for all files if you use webpack server (npm run dev)

---
+++ config/webpack.build.conf.js
@@ -0,0 +1,11 @@
+const merge = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base.config.js");
+
+const buildWebpackConfig = merge(baseWebpackConfig, {
+    mode: "production",
+
+});
+
+module.exports = new Promise((resolve, reject) => {
+    resolve(buildWebpackConfig);
+});
